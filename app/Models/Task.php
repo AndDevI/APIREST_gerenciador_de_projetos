d88b83b17df5b69f16a8eaae96829d72e1a2c541
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-30 14:11:01 -0300

    Defining relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['title', 'project_id', 'status', 'due_date'];
+
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
 }
